2f28a4bc09efe7690d265a126e0760b50f6e4eb5
Chloe Hall <erh8978@rit.edu>
2026-08-02 13:30:24 -0400

    01 - Instance completed

--- attachments/00_base_code.cpp
+++ attachments/00_base_code.cpp
@@ -20,12 +20,16 @@ class HelloTriangleApplication
 	{
 		initWindow();
 		initVulkan();
+		createInstance();
 		mainLoop();
 		cleanup();
 	}
 
   private:
 	GLFWwindow* window = nullptr;
+
+	vk::raii::Context  context;
+	vk::raii::Instance instance = nullptr;
 
 	void initWindow()
 	{
@@ -42,6 +46,41 @@ class HelloTriangleApplication
 
 	void initVulkan()
 	{
+		createInstance();
+	}
+
+	void createInstance()
+	{
+		// App name, engine, and version info struct
+		constexpr vk::ApplicationInfo appInfo{
+		    .pApplicationName   = "Hello Triangle",
+		    .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+		    .pEngineName        = "No Engine",
+		    .engineVersion      = VK_MAKE_VERSION(1, 0, 0),
+		    .apiVersion         = vk::ApiVersion14};
+
+		// Get the required instance extensions from GLFW
+		uint32_t glfwExtensionCount = 0;
+		auto     glfwExtensions     = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		// Check if required GLFW extensions are supported by the Vulkan implementation
+		auto extensionProperties = context.enumerateInstanceExtensionProperties();
+		for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+		{
+			if (std::ranges::none_of(extensionProperties,
+				[glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+				{ return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+			{
+				throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+			}
+		}
+
+		vk::InstanceCreateInfo createInfo{
+		    .pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions};
+
+		instance = vk::raii::Instance(context, createInfo);
 	}
 
 	void mainLoop()
